59a80c04f7ca893eab546c1614c1589b60d8cee2
Lautaro <fayanaslautaroagustin@gmail.com>
2023-04-13 21:28:29 +0200

    ValidatorForm

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
-import Usuarios from '../models/Usuario.js'
+import {check, validationResult} from 'express-validator';
+import Usuarios from '../models/Usuario.js';
 
 const formularioLogin = (req,res) => {
     res.render('auth/login', {
@@ -13,8 +14,26 @@ const formularioRegistro = (req,res) => {
 }
 
 const registrar = async (req,res) => {
-    const usuario = await Usuarios.create(req.body)
 
+    // Validacion del formulario
+    await check('nombre').notEmpty().withMessage('Debes completar el nombre').run(req)
+    await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+    await check('password').isLength({ min: 6 }).withMessage('El password debe contener al menos 6 caracteres').run(req)
+    await check('repetir_password').equals('password').withMessage('La password debe ser la misma').run(req)
+
+    let resultado = validationResult(req)
+
+   
+    // Verificar que el resultado esta vacio
+    if(!resultado.isEmpty()){
+        // No esta vacio, entonces hay errores
+        return res.render('auth/registro', {
+            pagina: 'Crear Cuenta',
+            errores: resultado.array()
+        })
+    }
+
+    const usuario = await Usuarios.create(req.body)
     res.json(usuario)
 }
 

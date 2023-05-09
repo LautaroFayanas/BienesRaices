e4fa1427cbd819756f9ca813174e95843014236c
Lautaro <fayanaslautaroagustin@gmail.com>
2023-05-09 16:49:49 +0200

    propertiesPrints

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -2,10 +2,23 @@ import { validationResult } from "express-validator"
 import { Precio, Categoria, Propiedad } from '../models/index.js'
 
 
-const admin = (req,res) => {
+const admin = async (req,res) => {
+
+    const { id } = req.usuario;
+    console.log(id);
+    const propiedades = await Propiedad.findAll({
+        where: {
+            usuarioId: id
+        },
+        include: [
+            {model: Categoria, as: 'categoria'},
+            {model: Precio, as: 'precio'}
+        ]
+    })
+
     res.render('propiedades/admin',{
         pagina: 'Mis propiedades',
-        
+        propiedades
     })
 }
 

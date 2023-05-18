31130a7975c65a49a94fce3e360c266f0e49f65b
Lautaro <fayanaslautaroagustin@gmail.com>
2023-05-18 10:53:37 +0200

    PagerInit

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -13,25 +13,45 @@ const admin = async (req,res) => {
         return res.redirect('/mis-propiedades?pagina=1')
     }
 
+    try {
 
     const { id } = req.usuario;
-    console.log(id);
+        
+    // Limite & offset Paginador
+    const limit = 10;
+    const offset = ((paginaActual * limit) - limit)
 
-    const propiedades = await Propiedad.findAll({
-        where: {
-            usuarioId: id
-        },
-        include: [
-            {model: Categoria, as: 'categoria'},
-            {model: Precio, as: 'precio'}
-        ]
-    })
+    const [propiedades , total] = await Promise.all([
+        Propiedad.findAll({
+            limit,
+            offset,
+            where: {
+                usuarioId: id
+            },
+            include: [
+                {model: Categoria, as: 'categoria'},
+                {model: Precio, as: 'precio'}
+            ]
+        }),
+        Propiedad.count({
+            where:{
+                usuarioId: id
+            }
+        })
+    ])
 
     res.render('propiedades/admin',{
         pagina: 'Mis propiedades',
         propiedades,
         csrfToken: req.csrfToken(),
-    })
+        paginas: Math.ceil(total / limit)
+    })    
+
+    } catch (error) {
+      console.log(error);  
+    }
+
+    
 }
 
 const crear = async (req,res) => {
